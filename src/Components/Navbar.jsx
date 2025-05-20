1ac578123be7c686b6d3e5c03d904fac640d0d73
fariya-sultana <fariya.sultana.dev@gmail.com>
2025-05-20 10:22:56 +0600

    register page added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router';
+import React, { use, useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router';
 import { CiLight } from "react-icons/ci";
 import { CiDark } from "react-icons/ci";
 import './navbar.css'
+import { FcBusinessman, FcUnlock } from 'react-icons/fc';
+import { AuthContext } from '../Contexts/AuthContext';
+import Swal from 'sweetalert2';
 
 const Navbar = () => {
+
+    const { user, logOut } = use(AuthContext);
     const [theme, setTheme] = useState("light");
 
     const links = <>
@@ -12,9 +17,21 @@ const Navbar = () => {
         <li><NavLink to={'/shareTip'}>Share a Garden Tip</NavLink></li>
         <li><NavLink to={'/gardeners'}>Explore Gardeners</NavLink></li>
         <li><NavLink to={'/myTip'}>My Tips</NavLink></li>
-        <li><NavLink to={'/login'}>LogIn</NavLink></li>
-        <li><NavLink to={'/register'}>SignUp</NavLink></li>
     </>
+
+    const handleLogout = () => {
+        logOut().then(() => {
+            Swal.fire({
+                position: "top-center",
+                icon: "success",
+                title: "Logout successful! 🎉",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }).catch(error => {
+            console.log(error.message)
+        })
+    }
 
     useEffect(() => {
         const savedTheme = localStorage.getItem("theme") || "light";
@@ -30,11 +47,11 @@ const Navbar = () => {
     }
 
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className='max-w-7xl mx-auto mt-6'>
             <div className="navbar bg-base-100 ">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className=" mr-3 lg:hidden">
+                        <div tabIndex={0} role="button" className=" mr-2 lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
                         <ul
@@ -43,17 +60,40 @@ const Navbar = () => {
                             {links}
                         </ul>
                     </div>
-                    <h2 className='text-xl md:text-2xl inline-flex items-center text-secondary'>
-                        <img className='w-10 mr-2' src="/public/plant-leaf.png" alt="" />Garden<span className='text-primary '>Hub</span>
+                    <h2 className='text-xl md:text-3xl inline-flex items-center text-secondary'>
+                        <img className='w-8 md:w-10 md:mr-2' src="/public/plant-leaf.png" alt="" />Garden<span className='text-primary '>Hub</span>
                     </h2>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="font-medium menu-horizontal px-1 inline-flex gap-6">
+                    <ul className="font-bold menu-horizontal px-1 inline-flex gap-6">
                         {links}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <button onClick={handleTheme} className='text-2xl '>{theme === 'light' ? <CiLight /> : <CiDark />} </button>
+                <div className="navbar-end items-center">
+                    {
+                        user ?
+                            <div className="dropdown dropdown-end md:dropdown-center mr-2">
+                                <div tabIndex={0} role="button" className="bg-primary rounded-full p-1 tooltip mt-2 " data-tip={user.displayName} >
+                                    {
+                                        (user.photoURL) ? <img src={user.photoURL} alt="" className='md:w-10 md:h-10 w-6 rounded-full' /> : <img src={user?.photoURL || "https://i.postimg.cc/15HJjdw8/3135823.png"} alt="" className='md:w-10 md:h-10 w-6  rounded-full' />
+                                    }
+                                </div>
+                                <div tabIndex={0} className="dropdown-content menu bg-white rounded-box z-1 w-54 p-2 shadow-sm text-center text-xl font-medium space-y-1 ">
+                                    <div className='bg-primary p-6 justify-items-center rounded-md mb-2'>
+                                        {
+                                            (user.photoURL) ? <img src={user.photoURL} alt="" className='md:w-20  rounded-full' /> : <img src={user?.photoURL || "https://i.postimg.cc/15HJjdw8/3135823.png"} alt="" className='w-20  rounded-full' />
+                                        }
+                                    </div>
+                                    <h3 className='text-secondary'>{user.displayName}</h3>
+                                    <button type='submit' onClick={handleLogout} className='btn btn-primary text-lg mt-2 font-bold inline-flex items-center gap-2 text-white'>Log Out <FcUnlock></FcUnlock></button>
+                                </div>
+                            </div> :
+                            <>
+                                <Link to={'/login'} className="btn bg-base-100 text-primary hover:bg-secondary hover:text-white font-bold btn-xs md:btn-sm md:mr-2 mr-1">Login</Link>
+                                <Link to={'/register'} className="btn bg-base-100 btn-xs text-primary md:btn-sm font-bold hover:bg-secondary hover:text-white md:mr-2 mr-1">Register</Link>
+                            </>
+                    }
+                    <button onClick={handleTheme} className='md:text-2xl text-xl'>{theme === 'light' ? <CiLight /> : <CiDark />} </button>
                 </div>
             </div>
         </div>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -74,24 +74,24 @@ const Register = () => {
     }
 
     return (
-        <div className="hero px-2 my-2">
+        <div className="hero px-2 mt-8">
 
-            <div className="card bg-white w-full max-w-sm shrink-0 shadow-2xl py-5" >
-                <h2 className='font-bold text-3xl text-center text-primary pb-3'>Register your account</h2>
+            <div className="card bg-secondary w-full max-w-sm shrink-0 shadow-2xl py-5" >
+                <h2 className='font-bold text-3xl text-center text-white pb-3'>Register  your  account</h2>
 
                 <div className="card-body py-0">
                     <form onSubmit={handleRegister} className="fieldset">
 
-                        <label className="text-primary font-medium text-lg label">Name</label>
+                        <label className="text-white font-medium text-lg label">Name</label>
                         <input type="text" className="bg-white text-primary border-primary input" placeholder="Your Name" name='name' required />
 
-                        <label className="text-primary font-medium text-lg label">Photo</label>
+                        <label className="text-white font-medium text-lg label">Photo</label>
                         <input type="text" className="bg-white text-primary border-primary input" placeholder="Photo-URL" name='photo' required />
 
-                        <label className="text-primary font-medium text-lg label">Email</label>
+                        <label className="text-white font-medium text-lg label">Email</label>
                         <input type="email" className="bg-white text-primary border-primary input" placeholder="Your Email" name='email' required />
 
-                        <label className="text-primary font-medium text-lg label">Password</label>
+                        <label className="text-white font-medium text-lg label">Password</label>
 
                         <input
                             type="password"
@@ -99,22 +99,23 @@ const Register = () => {
                             required
                             className="bg-white text-primary border-primary input validator "
                             placeholder="Your Password"
-                            minLength="6"
-                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-                            title="Must be more than characters, including number, lowercase letter, uppercase letter"
+                            minLength="8"
+                            pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}"
+
+                            title="Must be at least 8 characters, lowercase letter, uppercase letter and a special character"
                         />
                         <p className="validator-hint hidden text-center">
-                            Must be more than 6 character including
-                            at least one number, at least one lowercase letter, at least one uppercase letter
+                            Must be more than 8 character including
+                            at least one lowercase letter, at least one uppercase letter and a special character
                         </p>
 
-                        <button type='submit' className='btn mt-2 hover:bg-secondary text-lg bg-primary text-base-200' >Register</button>
+                        <button type='submit' className='btn mt-2 text-lg hover:bg-green-800 bg-green-900 text-white border-[#e5e5e5]' >Register</button>
 
-                        <p className='text-center text-secondary py-1 text-sm '>Already Have An Account? <Link className='font-bold text-secondary hover:underline' to={'/login'}>Login</Link></p>
+                        <p className='text-center text-white py-1 text-sm '>Already Have An Account? <Link className='font-bold text-white hover:underline' to={'/login'}>Login</Link></p>
 
                     </form>
 
-                    <button type='submit' onClick={handleGoogleBtn} className="btn hover:bg-secondary hover:text-base-200 bg-base-200 border-[#e5e5e5] text-primary">
+                    <button type='submit' onClick={handleGoogleBtn} className="btn hover:bg-green-800 bg-green-900 border-[#e5e5e5] text-white">
                         <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                         Login with Google
                     </button>
